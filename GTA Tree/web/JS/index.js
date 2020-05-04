f536fed1e5a003e1d35165fff83398e21f84ba61
Amitabh Priyadarshi <amitabh.priyadarshi@gmail.com>
2020-05-04 18:53:52 -0400

    Working card

--- GTA Tree/web/JS/index.js
+++ GTA Tree/web/JS/index.js
@@ -1,4 +1,3 @@
-
 var lastEventListener = null;
 var svgload = function () {
     var embed = document.createElement('object');
@@ -14,7 +13,6 @@ var svgload = function () {
             controlIconsEnabled: true
         });
         gta();
-
     };
     embed.addEventListener('load', lastEventListener);
     console.log("done");
@@ -26,15 +24,11 @@ var svg = function () {
     return a;
 };
 
-
 window.onload = function () {
     svgload();
 };
 
-
-
 var gta = function () {
-
     svg().getElementById('Fam_Names').style.cursor = "pointer";
     var lbl = svg().getElementById('Fam_Names');
     for (let i = 0; i < lbl.children.length; i++) {
@@ -42,37 +36,45 @@ var gta = function () {
         lbl.children[i].setAttribute("text", txt);
     }
     lbl.onclick = function (event) {
-
         var text = event.target.getAttribute('text');
-        //alert(text);
-//        document.getElementById("info").innerHTML = text;
-        //document.getElementById("info").innerHTML = text+"<img src='./weblogos/"+text+".short.fa.png'/>";
-        document.getElementById("weblogo").innerHTML = text + "<div id='' style='overflow-x:scroll; width:900px;'><img src='weblogos/" +text+ ".short.fa.png' height=80px></div>";
+        document.getElementById("weblogo").innerHTML ="<div id='myModal' class='modal custom-modal'>"
+                                                +"<section class='mb-4'>"
+                                                    +" <div class='container'>"
+                                                        +"<div class='card-columns'>"
+                                                            +"<div class='card border-info mb-3' style='max-width: 40rem; min-width: 25rem;'>"
+                                                                +"<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"
+                                                                        +"<span aria-hidden='true' class='white-text'>&times;</span>"
+                                                                +"</button>"
+                                                                +"<div class='card-header bg-background content-cente'>"
+                                                                    +text
+                                                                +"</div>"
+                                                                +"<div class='card-body'>"
+                                                                    +"<h5 class='card-title'>Title</h5>"
+                                                                    +"<h6 class='card-subtitle mb-2 text-muted'>Subtitle</h6>"
+                                                                    +"<p class='card-text'>example text to build on the card title and make up the bulk of the card'sexample text to build on the card title and make up the bulk of the card'sexample text to build on the card title and make up the bulk of the card'sexample text to build on the card title and make up the bulk of the card'sexample text to build on the card title and make up the bulk of the card's content.</p>"
+                                                                    +"<ul class='list-group list-group-flush'>"
+                                                                        +"<li class='list-group-item'>section1</li>"
+                                                                        +"<li class='list-group-item'>section2</li>"
+                                                                        +"<li class='list-group-item'>section3</li>"
+                                                                    +"</ul>"
+                                                                +"</div>"
+                                                                +"<div class='card-img-top' id='' style='overflow-x:scroll; width:400px;'><img src='weblogos/" +text+ ".short.fa.png' height=80px></div>"
+                                                                +"<div class='card-footer bg-light border-info'>"
+                                                                    +"<a href='#' class='card-link'>Link</a>"
+                                                                    +"<a href='#' class='card-link'>Download</a>"
+                                                                +"</div>"
+                                                            +"</div>"
+                                
+                                                        +"</div>"
+                                                    +"</div>"
+                                                +"</section>"
+                                            +"</div>";
 
-        /*
-         if (genesNfamily[i].genes === text) {
-         document.getElementById("info").innerHTML = text + "<div id='' style='overflow-x:scroll; width:900px;'><img src='images/" + genesNfamily[i].weblogo + "' height=80px></div>";
-         var brn = svg().getElementById('BRANCHES');
-         var allgenes = [];
-         for (let i = 0; i < brn.children.length; i++) {
-         brn.children[i].setAttribute('fill', '#cccccc');
-         allgenes.push(brn.children[i].id);
-         }
-         var circle = svg().getElementById('CIRCLES');
-         var allcircle = [];
-         for (let i = 0; i < circle.children.length; i++) {
-         circle.children[i].setAttribute('fill', '#CECECE');
-         circle.children[i].setAttribute('r', '4');
-         allcircle.push(circle.children[i].id);
-         
-         }
-         }
-         */
-
-
-
+        var modal = document.getElementById("myModal");
+        modal.style.display = "block";
+        var span = document.getElementsByClassName("close")[0];
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
     };
-
 };
-
-
