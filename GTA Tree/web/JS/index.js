029bd929918764d627fa5c934bd2c61925dbbe91
Amitabh Priyadarshi <amitabh.priyadarshi@gmail.com>
2020-05-09 18:52:05 -0400

    Card Complete

--- GTA Tree/web/JS/index.js
+++ GTA Tree/web/JS/index.js
@@ -1,7 +1,3 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-});
-
 var lastEventListener = null;
 var svgload = function () {
     var embed = document.createElement('object');
@@ -59,38 +55,24 @@ var gta = function () {
                 msg="failed"
             }
         });
-        
-        var subfamily="<p class='card-text' style='font-size: 10px; color: #000;padding-left: 20px;'>";
+
+        var subfamily="<table class='scrollable'>"
+                                +"<tr style='font-size: 10px;'>"
+                                    +"<th>Subfamily</th>"
+                                    +"<th> | FastaGTADomain</th>"
+                                    +"<th> | FastaFullSequence</th>"
+                                    +"<th> | Tables</th>"
+                                +"</tr>";       
         $.each(jsondata[index].subfamilies, function(i, value) {          
             var download="";
             $.each(value.downloads, function(j, val) {          
-                download += val.name+":<a href='"+val.nr+"' title='NR'><i class='fas fa-file-download' style='color: #ff5200;font-size: 17px;'></i></a>&nbsp;<a href='"+val.uniprot+"' title='Uniprot'><i class='fas fa-file-download' style='color: #084177;font-size: 17px;'></i></a>";
-           // alert(download);
+                download += "<td class='center'><a href='"+val.nr+"' title='NR'><i class='fas fa-file-download' style='color:#ff5200;font-size:17px;'></i></a>&nbsp;<a href='"+val.uniprot+"' title='Uniprot'><i class='fas fa-file-download' style='color:#084177;font-size:17px;'></i></a></td>";
             });
-            subfamily+="<span data-html='true' data-toggle='popover' data-placement='top' data-original-title='Download' data-content='hello'>"+value.name+", </span>";
-            
+            subfamily+="<tr><td style='font-size: 9px; color: #000;' nowrap><div><span>"+value.name+"</span></div></td>"+download+"</tr>"; 
         });
-        //subfamily = subfamily.substring(0,subfamily.length-2);
-        subfamily+="</p>";
-        ///////dynamically created main problem hai
-        //document.getElementById("test").innerHTML = subfamily;
-        function Popx(id) {
-                $('#' + id).popover({
-                //$('#yo').popover({
-                    html: true,
-                    trigger: 'focus',
-                    content: 'yoyo',
-                    title: 'Download2',
-                    placement: 'top'
-                }).hover(function (e) {
-                    $(this).popover('focus');
-                });
-        }
-        
-        
-        document.getElementById("test").innerHTML = "<div id='yo'><span tabindex='6' data-container='body' data-html='true' data-toggle='popover' data-placement='top' data-trigger='focus' data-original-title='Download1' data-content='<b>hello</b>'>pop2</span></div>";
-        Popx("yo");
-        
+        subfamily+="</table>";
+
+
         var domainOrganization="<table class='scrollable'>"
                                     +"<tr style='font-size: 10px;'>"
                                         +"<th>Domains</th>"
@@ -98,28 +80,28 @@ var gta = function () {
                                         +"<th>Count</th>"
                                     +"</tr>";
         $.each(jsondata[index].domainOrganization, function(i, val) {          
-            domainOrganization+="<tr style='font-size: 10px; color: #000;'>"
+            domainOrganization+="<tr style='font-size: 9px; color: #000;'>"
                                     +"<td class='left'>"+val.domains+"</td>"
                                     +"<td class='center' style='color: #c00;'>"+val.percent+"</td>"
                                     +"<td class='center'>"+val.count+"</td>"
                             +"</tr>";
         });
         domainOrganization+="</table class='table'>"
-       
-       var taxonomicDistribution="<table class='scrollable'>"
-                                        +"<tr style='font-size: 10px;'>"
-                                            +"<th>SubFamily</th>"
-                                            +"<th>Viruses</th>"
-                                            +"<th>Archaea</th>"
-                                            +"<th>Bacteria</th>"
-                                            +"<th>Protista</th>"
-                                            +"<th>Fungi</th>"
-                                            +"<th>Viridiplantae</th>"
-                                            +"<th>Metazoa</th>"
-                                            +"<th>Unknown</th>"
-                                        +"</tr>";
+
+        var taxonomicDistribution="<table class='scrollable'>"
+                                         +"<tr style='font-size: 10px;'>"
+                                             +"<th>SubFamily</th>"
+                                             +"<th>Virusus</th>"
+                                             +"<th>Archaea</th>"
+                                             +"<th>Bacteria</th>"
+                                             +"<th>Protista</th>"
+                                             +"<th>Fungi</th>"
+                                             +"<th>Plantae</th>"
+                                             +"<th>Metazoa</th>"
+                                             +"<th>Unk</th>"
+                                         +"</tr>";
         $.each(jsondata[index].taxonomicDistribution, function(i, val) {          
-            taxonomicDistribution+="<tr style='font-size: 10px; color: #000;'>"
+            taxonomicDistribution+="<tr style='font-size: 9px; color: #000;'>"
                                     +"<td nowrap><div><span>"+val.subfamilyName+"</span></div></td>"
                                     +"<td class='center'>"+val.viruses+"</td>"
                                     +"<td class='center'>"+val.Archaea+"</td>"
@@ -131,46 +113,40 @@ var gta = function () {
                                     +"<td class='center'>"+val.Unknown+"</td></tr>";
         });
         taxonomicDistribution+="</table>"
-        
+
         var downloads="";
         $.each(jsondata[index].downloads, function(i, val) {          
-            downloads+=val.name+":  <a href='"+val.nr+"' title='NR'><i class='fas fa-file-download' style='color: #ff5200;font-size: 17px;'></i></a>&nbsp;<a href='"+val.uniprot+"' title='Uniprot'><i class='fas fa-file-download' style='color: #084177;font-size: 17px;'></i></a>&nbsp;";
+            downloads+=val.name+":  <a href='"+val.nr+"' title='NR'><i class='fas fa-file-download' style='color: #ff5200;font-size: 17px;'></i></a>&nbsp;<a href='"+val.uniprot+"' title='Uniprot'><i class='fas fa-file-download' style='color: #084177;font-size: 17px;'></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
         });
-        
-       
+        downloads = downloads.substring(0,downloads.length-6);
+
         document.getElementById("weblogo").innerHTML ="<div id='myModal' class='modal custom-modal'>"
                                                 +"<section class='mb-4'>"
-                                        
                                                     +" <div class='container'>"
                                                         +"<div class='card-columns'>"
-                                                            +"<div class='card border-info mb-3' style='max-width: 40rem; min-width: 25rem; max-height: 41rem; min-height: 41rem;'>"
+                                                            +"<div class='card border-info mb-3' style='max-width: 25rem; min-width: 25rem; max-height: 42rem; min-height: 42rem;'>"
                                                                 +"<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"
                                                                         +"<span aria-hidden='true' class='white-text'>&times;</span>"
                                                                 +"</button>"
                                                                 +"<div class='card-header bg-background content-cente' style='height:50px;'>"
                                                                     +"<h4 class='card-title'><i class='fas fa-dna'></i> <b>"+jsondata[index].familyName+"</b><div class='tooltip'>Hover over me<span class='tooltiptext'>Tooltip text</span></div> </h4>"
                                                                 +"</div>"
-                                                                +"<div class='card-body scrollableDiv' style='padding-top: 10px;'>"
+                                                                +"<div class='card-body scrollableDiv' style='padding-top: 10px;margin-left: -10px;margin-right: -10px;max-height: 463px; min-height: 463px;'>"
                                                                     +"<h6 class='card-subtitle mb-2 text-dark'>Description: </h6>"
-                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;padding-left: 20px;'>"+jsondata[index].description+"</p>"
-                                                                    +"<p class='card-subtitle mb-2 text-dark' style='font-size: 10px; color: #000; padding-left: 20px;'>Mechanism: "+jsondata[index].mechanism+"</p>"    
-                                                                    +"</br>"                                                  
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark' '>Subfamilies </h6>"
-                                                                    +"<p class='card-text' style='font-size: 12px; color: #000; padding-left: 20px;'>"+subfamily+"</p"
-                                                                    +"</br>"                                                    
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'  >Domain Organization <a href='"+jsondata[index].domainInformation+"' class='card-link' title='Domain Information'><i class='fas fa-file-download' style='color: #16817a;font-size: 17px;'></i></a></h6>"
-                                                                    +domainOrganization
-                                                                    +"</br>"                                                    
+                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -10px;padding-left: 10px;'>"+jsondata[index].description+"</br><b>Mechanism: </b>"+jsondata[index].mechanism+"</p>"    
+                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Subfamilies </h6>"
+                                                                    +"<p class='card-text' style='color: #000;margin-top: -15px;'>"+subfamily+"</p>"
+                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Domain Organization <a href='"+jsondata[index].domainInformation+"' class='card-link' title='Domain Information'><i class='fas fa-file-download' style='color: #16817a;font-size: 17px;'></i></a></h6>"
+                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;10px;margin-top: -15px;'>"+domainOrganization  +"</p>"                                        
                                                                     +"<h6 class='card-subtitle mb-2 text-dark'>Taxonomic Distribution </h6>"
-                                                                    +taxonomicDistribution
+                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -15px;'>"+taxonomicDistribution+"</p>"
                                                                 +"</div>"
-                                                                +"<h6 class='card-subtitle mb-2 text-dark' style='padding-left: 20px;'>Family Alignment</h6>"
-                                                                +"<div class='card-img-top' id='' style='overflow-x:scroll; width:397px;'><img src='"+jsondata[index].familyAlignment+"' height=80px></div>"
-                                                                +"<div class='card-footer bg-light border-info' style='font-size: 12px;'>"
-                                                                    +downloads
+                                                                +"<div class='card-footer bg-white border-info' style='font-size: 10px;'>"
+                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Family Alignment</h6>"
+                                                                    +"<div class='card-img-top' id='' style='overflow-x:scroll; width:397px;margin-left:-19px;margin-top:-5px;'><img src='"+jsondata[index].familyAlignment+"' height=80px></div>"
+                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: 5px;'>"+downloads+"</p>"
                                                                 +"</div>"
                                                             +"</div>"
-                                
                                                         +"</div>"
                                                     +"</div>"
                                                 +"</section>"
