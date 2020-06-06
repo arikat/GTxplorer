c42275f8569ecbeb6c0a8e3e754e0e806ddcf26b
Amitabh Priyadarshi <amitabh.priyadarshi@gmail.com>
2020-06-06 13:49:13 -0400

    Direct Download function is added Clade and Family is done and all bugs fixed

--- GTA Tree/web/JS/index.js
+++ GTA Tree/web/JS/index.js
@@ -56,7 +56,6 @@ var gta = function () {
     for (let i = 0; i < lbl_clade.children.length; i++) {
         var txt = lbl_clade.children[i].textContent;
         if(txt!=""){
-            console.log(txt);
             lbl_clade.children[i].setAttribute("text", txt);
         }
     }
@@ -71,84 +70,95 @@ var gta = function () {
 };
 
 var card = function(GTAname, jsondata){
-        var index=0;
-        var msg="";
-        $.each(jsondata, function(i, val) {
-            console.log(val+" --  "+GTAname);
-            if(val.familyName === GTAname){
-                index=i;
-                msg="success";
-                return false;
-            }else{
-                msg="failed";
+    var index=0;
+    var msg="";
+    var cladeName = GTAname;
+    cladeName = cladeName.substring(0,cladeName.length-2);
+
+    var famName = GTAname;
+    famName = famName.substring(0,2);
+
+    $.each(jsondata, function(i, val) {
+        //console.log(val+" --  "+GTAname);
+        if(val.familyName === GTAname){
+            index=i;
+            msg="success";
+            return false;
+        }else{
+            msg="failed";
+        }
+    });
+    if (msg === "success") {
+        ///////Family////////
+        var subfamily = "";
+        if (jsondata[index].subfamilies === null || jsondata[index].subfamilies === undefined || jsondata[index].subfamilies === '' || jsondata[index].subfamilies.length === 0) {
+            subfamily = "<span style='font-size: 9px; color: #000;'>No Data Found!</span>";
+        } else {
+            subfamily = "<table class='scrollable'>"
+                    + "<tr style='font-size: 10px;'>"
+            if (cladeName === "Clade") {
+                subfamily += "<th>Family</th>";
             }
-        });
-        if(msg === "success"){
-        var subfamily="<table class='scrollable'>"
-                                +"<tr style='font-size: 10px;'>"
-                                    +"<th>Subfamily</th>"
-                                    +"<th> | FastaGTADomain</th>"
-                                    +"<th> | FastaFullSequence</th>"
-                                    +"<th> | Tables</th>"
-                                +"</tr>";       
-        $.each(jsondata[index].subfamilies, function(i, value) {          
-            var download="";
-            $.each(value.downloads, function(j, val) {          
-                download += "<td class='center'><a href='"+val.nr+"' title='NR'><i class='fas fa-file-download' style='color:#ff5200;font-size:17px;'></i></a>&nbsp;<a href='"+val.uniprot+"' title='Uniprot'><i class='fas fa-file-download' style='color:#084177;font-size:17px;'></i></a></td>";
+            if (famName === "GT") {
+                subfamily += "<th>Subfamily</th>";
+            }
+            subfamily += "<th> | FastaGTADomain</th>"
+                    + "<th> | FastaFullSequence</th>"
+                    + "<th> | Tables</th>"
+                    + "</tr>";
+            $.each(jsondata[index].subfamilies, function (i, value) {
+                var download = "";
+                $.each(value.downloads, function (j, val) {
+                    download += "<td class='center'><a href='" + val.nr + "' title='NR' download><i class='fas fa-file-download' style='color:#ff5200;font-size:17px;'></i></a>&nbsp;<a href='" + val.uniprot + "' title='Uniprot' download><i class='fas fa-file-download' style='color:#084177;font-size:17px;'></i></a></td>";
+                });
+                subfamily += "<tr><td style='font-size: 9px; color: #000;' nowrap><div><span>" + value.name + "</span></div></td>" + download + "</tr>";
             });
-            subfamily+="<tr><td style='font-size: 9px; color: #000;' nowrap><div><span>"+value.name+"</span></div></td>"+download+"</tr>"; 
+            subfamily += "</table>";
+        }
+        ///////Domain Organization////////
+        var domainOrganization = "";
+        if (jsondata[index].domainOrganization === null || jsondata[index].domainOrganization === undefined || jsondata[index].domainOrganization === '' || jsondata[index].domainOrganization.length === 0) {
+            domainOrganization = "<br>No Domain Organization Found!";
+        } else {
+            domainOrganization = "<table class='scrollable'>"
+                    + "<tr style='font-size: 10px;'>"
+                    + "<th>Domains</th>"
+                    + "<th>Percent</th>"
+                    + "<th>Count</th>"
+                    + "</tr>";
+            $.each(jsondata[index].domainOrganization, function (i, val) {
+                domainOrganization += "<tr style='font-size: 9px; color: #000;'>"
+                        + "<td class='left'>" + val.domains + "</td>"
+                        + "<td class='center' style='color: #c00;'>" + val.percent + "</td>"
+                        + "<td class='center'>" + val.count + "</td>"
+                        + "</tr>";
+            });
+            domainOrganization += "</table class='table'>"
+        }
+        ///////Taxonomic Distributionain////////
+        var taxonomicDistribution = "<table class='scrollable'>"
+                + "<tr style='font-size: 10px;'>"
+                + "<th>Archaea</th>"
+                + "<th>Bacteria</th>"
+                + "<th>Fungi</th>"
+                + "<th>Metazoa</th>"
+                + "<th>Protista</th>"
+                + "<th>Unknown</th>"
+                + "<th>Viridiplantae</th>"
+                + "<th>Viruses</th>"
+                + "</tr>"
+                + "<tr style='font-size: 10px; color: #000;'>";
+        $.each(jsondata[index].taxonomicDistribution, function (i, val) {
+            taxonomicDistribution += "<td nowrap class='center'><div><span>" + val + "</span></div></td>";
         });
-        subfamily+="</table>";
-
-
-        var domainOrganization="<table class='scrollable'>"
-                                    +"<tr style='font-size: 10px;'>"
-                                        +"<th>Domains</th>"
-                                        +"<th>Percent</th>"
-                                        +"<th>Count</th>"
-                                    +"</tr>";
-        $.each(jsondata[index].domainOrganization, function(i, val) {          
-            domainOrganization+="<tr style='font-size: 9px; color: #000;'>"
-                                    +"<td class='left'>"+val.domains+"</td>"
-                                    +"<td class='center' style='color: #c00;'>"+val.percent+"</td>"
-                                    +"<td class='center'>"+val.count+"</td>"
-                                +"</tr>";
+        taxonomicDistribution += "</tr></table>"
+        ///////Downloads///////////////
+        var downloads = "";
+        $.each(jsondata[index].downloads, function (i, val) {
+            downloads += val.name + ":  <a href='" + val.nr + "' title='NR' download><i class='fas fa-file-download' style='color: #ff5200;font-size: 17px;'></i></a>&nbsp;<a href='" + val.uniprot + "' title='Uniprot' download><i class='fas fa-file-download' style='color: #084177;font-size: 17px;'></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
         });
-        domainOrganization+="</table class='table'>"
-
-        var taxonomicDistribution="<table class='scrollable'>"
-                                         +"<tr style='font-size: 10px;'>"
-                                             +"<th>SubFamily</th>"
-                                             +"<th>Virusus</th>"
-                                             +"<th>Archaea</th>"
-                                             +"<th>Bacteria</th>"
-                                             +"<th>Protista</th>"
-                                             +"<th>Fungi</th>"
-                                             +"<th>Plantae</th>"
-                                             +"<th>Metazoa</th>"
-                                             +"<th>Unk</th>"
-                                         +"</tr>";
-        $.each(jsondata[index].taxonomicDistribution, function(i, val) {          
-            taxonomicDistribution+="<tr style='font-size: 9px; color: #000;'>"
-                                    +"<td nowrap><div><span>"+val.subfamilyName+"</span></div></td>"
-                                    +"<td class='center'>"+val.viruses+"</td>"
-                                    +"<td class='center'>"+val.Archaea+"</td>"
-                                    +"<td class='center'>"+val.Bacteria+"</td>"
-                                    +"<td class='center'>"+val.Protista+"</td>"
-                                    +"<td class='center'>"+val.Fungi+"</td>"
-                                    +"<td class='center'>"+val.Viridiplantae+"</td>"
-                                    +"<td class='center'>"+val.Metazoa+"</td>"
-                                    +"<td class='center'>"+val.Unknown+"</td></tr>";
-        });
-        taxonomicDistribution+="</table>"
-
-        var downloads="";
-        $.each(jsondata[index].downloads, function(i, val) {          
-            downloads+=val.name+":  <a href='"+val.nr+"' title='NR'><i class='fas fa-file-download' style='color: #ff5200;font-size: 17px;'></i></a>&nbsp;<a href='"+val.uniprot+"' title='Uniprot'><i class='fas fa-file-download' style='color: #084177;font-size: 17px;'></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-        });
-        downloads = downloads.substring(0,downloads.length-6);
-
-        document.getElementById("weblogo").innerHTML ="<div id='myModal' class='modal custom-modal'>"
+        downloads = downloads.substring(0, downloads.length - 6);
+        var cardData="<div id='myModal' class='modal custom-modal'>"
                                                 +"<section class='mb-4'>"
                                                     +" <div class='container'>"
                                                         +"<div class='card-columns'>"
@@ -161,11 +171,27 @@ var card = function(GTAname, jsondata){
                                                                 +"</div>"
                                                                 +"<div class='card-body scrollableDiv' style='padding-top: 10px;margin-left: -10px;margin-right: -10px;max-height: 460px; min-height: 460px;'>"
                                                                     +"<h6 class='card-subtitle mb-2 text-dark'>Description: </h6>"
-                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -10px;padding-left: 10px;'>"+jsondata[index].description+"</br><b>Mechanism: </b>"+jsondata[index].mechanism+"</p>"    
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Subfamilies </h6>"
-                                                                    +"<p class='card-text' style='color: #000;margin-top: -15px;'>"+subfamily+"</p>"
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Domain Organization <a href='"+jsondata[index].domainInformation+"' class='card-link' title='Domain Information'><i class='fas fa-file-download' style='color: #16817a;font-size: 17px;'></i></a></h6>"
-                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;10px;margin-top: -15px;'>"+domainOrganization  +"</p>"                                        
+                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -10px;padding-left: 10px;'>"+jsondata[index].description+"</br><b>Mechanism: </b>"+jsondata[index].mechanism+"</p>";
+                                                            
+                                                                if(cladeName === "Clade"){
+                                                                    cardData +="<h6 class='card-subtitle mb-2 text-dark'>Families </h6>";
+                                                                 }
+                                                                if(famName === "GT"){
+                                                                   cardData +="<h6 class='card-subtitle mb-2 text-dark'>SubFamilies </h6>";
+                                                                }   
+                                                            cardData+="<p class='card-text' style='color: #000;margin-top: -15px;'>"+subfamily+"</p>"
+                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Domain Organization";
+                                                            
+                                                                if(cladeName === "Clade"){
+                                                                    cardData +="</h6>";
+                                                                 }
+                                                                if(famName === "GT"){
+                                                                   cardData +=" <a href='"+jsondata[index].domainInformation+"' class='card-link' title='Domain Information' download><i class='fas fa-file-download' style='color: #16817a;font-size: 17px;'></i></a></h6>";
+                                                                } 
+
+        
+        
+                                                               cardData+="<p class='card-text' style='font-size: 10px; color: #000;10px;margin-top: -15px;'>"+domainOrganization  +"</p>"                                        
                                                                     +"<h6 class='card-subtitle mb-2 text-dark'>Taxonomic Distribution </h6>"
                                                                     +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -15px;'>"+taxonomicDistribution+"</p>"
                                                                 +"</div>"
@@ -179,6 +205,7 @@ var card = function(GTAname, jsondata){
                                                     +"</div>"
                                                 +"</section>"
                                             +"</div>";
+        document.getElementById("weblogo").innerHTML = cardData;
 
         var modal = document.getElementById("myModal");
         modal.style.display = "block";
