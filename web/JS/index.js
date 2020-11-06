3b8c96279944718b792ebabfb79e36b1a07a11c1
Rahil Taujale <rtaujale@uga.edu>
2020-11-05 20:15:49 -0500

    Design Updates for main webpage.

--- web/JS/index.js
+++ web/JS/index.js
@@ -190,25 +190,25 @@ var card = function(GTAname, jsondata){
                                                 +"<section class='mb-4'>"
                                                     +" <div class='container'>"
                                                         +"<div class='card-columns'>"
-                                                            +"<div class='card border-info mb-3' style='max-width: 25rem; min-width: 25rem; max-height: 42rem; min-height: 42rem;'>"
-                                                                +"<button type='button' class='close' data-dismiss='modal' aria-label='Close'>"
+                                                            +"<div class='card card-border mb-3' style='max-width: 25rem; min-width: 25rem; max-height: 41rem; min-height: 41rem;'>"
+                                                                +"<button type='button' class='close  mr-1' data-dismiss='modal' aria-label='Close'>"
                                                                         +"<span aria-hidden='true' class='white-text'>&times;</span>"
                                                                 +"</button>"
-                                                                +"<div class='card-header bg-background content-cente' style='height:50px;'>"
-                                                                    +"<h4 class='card-title'><i class='fas fa-dna'></i> <b>"+jsondata[index].familyName+"</b><div class='tooltip'>Hover over me<span class='tooltiptext'>Tooltip text</span></div> </h4>"
+                                                                +"<div class='card-header card-header-custom content-center' style='height:50px;'>"
+                                                                    +"<h4 class='title-color'><i class='fas'></i> <b>"+jsondata[index].familyName+"</b><div class='tooltip'>Hover over me<span class='tooltiptext'>Tooltip text</span></div> </h4>"
                                                                 +"</div>"
-                                                                +"<div class='card-body scrollableDiv' style='padding-top: 10px;margin-left: -10px;margin-right: -10px;max-height: 460px; min-height: 460px;'>"
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Description: </h6>"
+                                                                +"<div class='card-body card-body-custom scrollableDiv'>"
+                                                                    +"<h6 class='card-subtitle mb-3 text-dark'>Description </h6>"
                                                                     +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -10px;padding-left: 10px;'>"+jsondata[index].description+"</br><b>Mechanism: </b>"+jsondata[index].mechanism+"</p>";
                                                             
                                                                 if(cladeName === "Clade"){
-                                                                    cardData +="<h6 class='card-subtitle mb-2 text-dark'>Families </h6>";
+                                                                    cardData +="<h6 class='card-subtitle mb-3 text-dark'>Families </h6>";
                                                                  }
                                                                 if(famName === "GT"){
-                                                                   cardData +="<h6 class='card-subtitle mb-2 text-dark'>SubFamilies </h6>";
+                                                                   cardData +="<h6 class='card-subtitle mb-3 text-dark'>SubFamilies </h6>";
                                                                 }   
-                                                            cardData+="<p class='card-text' style='color: #000;margin-top: -15px;'>"+subfamily+"</p>"
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Domain Organization";
+                                                            cardData+="<p class='card-text' style='color: #000;margin-top: -10px;'>"+subfamily+"</p>"
+                                                                    +"<h6 class='card-subtitle mb-3 text-dark'>Domain Organization";
                                                             
                                                                 if(cladeName === "Clade"){
                                                                     cardData +="</h6>";
@@ -219,12 +219,12 @@ var card = function(GTAname, jsondata){
 
         
         
-                                                               cardData+="<p class='card-text' style='font-size: 10px; color: #000;10px;margin-top: -15px;'>"+domainOrganization  +"</p>"                                        
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Taxonomic Distribution </h6>"
-                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -15px;'>"+taxonomicDistribution+"</p>"
+                                                               cardData+="<p class='card-text' style='font-size: 10px; color: #000;10px;margin-top: -10px;'>"+domainOrganization  +"</p>"                                        
+                                                                    +"<h6 class='card-subtitle mb-3 text-dark'>Taxonomic Distribution </h6>"
+                                                                    +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: -10px;'>"+taxonomicDistribution+"</p>"
                                                                 +"</div>"
-                                                                +"<div class='card-footer bg-white border-info' style='font-size: 10px;'>"
-                                                                    +"<h6 class='card-subtitle mb-2 text-dark'>Family Alignment</h6>"
+                                                                +"<div class='card-footer card-footer-custom bg-white card-border'>"
+                                                                    +"<h6 class='card-subtitle mb-1 text-dark'>Family Alignment</h6>"
                                                                     +"<div class='card-img-top' id='' style='overflow-x:scroll; width:397px;margin-left:-19px;margin-top:-5px;'><img src='"+jsondata[index].familyAlignment+"' height=80px></div>"
                                                                     +"<p class='card-text' style='font-size: 10px; color: #000;margin-top: 5px;'>"+downloads+"</p>"
                                                                 +"</div>"
